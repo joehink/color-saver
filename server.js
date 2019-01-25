09823b8f4540e4f86dece0ef98d8a228b5b596dd
Joe Hinkley <joehhinkley@gmail.com>
2019-01-25 14:54:24 -0800

    added initial middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 // DEPENDENCIES
 const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 // CONFIGURATION
 const app = express();
@@ -8,12 +10,19 @@ const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
 // MIDDLEWARE
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
 
 // DATABASE
-
+mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+  console.log('Connected to db');
+})
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
 })
+
+// ROUTES
