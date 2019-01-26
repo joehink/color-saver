d8403cb6ab6fff9505bf7eee87ba6eee91f5237d
Joe Hinkley <joehhinkley@gmail.com>
2019-01-25 18:35:09 -0800

    added form to new project page

--- controllers/projects_controller.js
+++ controllers/projects_controller.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+// Show projects belonging to user
 router.get('/', (req, res) => {
   res.render('projects/index.ejs');
+});
+
+// New project
+router.get('/new', (req, res) => {
+  res.render('projects/new.ejs');
+});
+
+// Create Project
+router.post('/', (req, res) => {
+  res.send('created');
 })
 
 module.exports = router;
