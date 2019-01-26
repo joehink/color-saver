c4dbbf4147deda555971a9965207dfb4ce96fdef
Joe Hinkley <joehhinkley@gmail.com>
2019-01-25 19:16:26 -0800

    create projects unique to each user

--- controllers/projects_controller.js
+++ controllers/projects_controller.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+ 
+var ObjectID = require('mongodb').ObjectID;
+
+const Project = require('../models/projects');
 
 // Show projects belonging to user
-router.get('/', (req, res) => {
-  res.render('projects/index.ejs');
+router.get('/', async (req, res) => {
+  const userProjects = await Project.find({
+    createdBy: new ObjectID(req.session.currentUser._id)
+  });
+  console.log('currentUser', req.session.currentUser._id);
+  console.log('projects', userProjects);
+  res.render('projects/index.ejs', { projects: userProjects });
 });
 
 // New project
@@ -12,8 +21,19 @@ router.get('/new', (req, res) => {
 });
 
 // Create Project
-router.post('/', (req, res) => {
-  res.send('created');
+router.post('/', async (req, res) => {
+  try {
+    // Assign createdBy property eaqual to currentUser id
+    req.body.createdBy = req.session.currentUser._id;
+    console.log(req.body);
+    // Add project to db
+    const createdProject = await Project.create(req.body);
+
+    res.redirect('/projects');
+  } catch (err) {
+    console.error(err);
+    res.redirect('/projects');
+  }
 })
 
 module.exports = router;
