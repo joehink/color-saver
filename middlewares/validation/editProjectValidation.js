368e74ec475ea261dd80f2b3bf38af22ea1c34ed
Joe Hinkley <joehhinkley@gmail.com>
2019-01-28 13:17:39 -0800

    styled error message in forms

--- middlewares/validation/editProjectValidation.js
+++ middlewares/validation/editProjectValidation.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
   if (req.body.title.length === 0) {
     req.flash('error', 'Project must have a title.')
-    res.redirect(`/projects/${req.params.id}/edit`);
+    return res.redirect(`/projects/${req.params.id}/edit`);
   }
   next();
 }
