962332194ddad1bbb7b0511d13d07ae28cd2e786
Joe Hinkley <joehhinkley@gmail.com>
2019-01-29 11:00:06 -0800

    Added comments to server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 // DEPENDENCIES
 const express = require('express');
-const expressValidator = require('express-validator');
 const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const randomGradient = require('./middlewares/randomGradient');
 const requireLogin = require('./middlewares/requireLogin');
 const session = require('express-session');
-
-const randomGradient = require('./middlewares/randomGradient');
 
 // CONFIGURATION
 const app = express();
@@ -24,7 +22,6 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }));
-
 app.use(flash());
 
 
@@ -32,30 +29,37 @@ app.use(flash());
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 mongoose.connection.once('open', () => {
   console.log('Connected to db');
-})
+});
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
-})
+});
 
 // ROUTES
 app.get('/', randomGradient, (req, res) => {
+  // If there is a user logged in
   if (req.session.currentUser) {
+    // Redirect to projects index
     res.redirect('/projects');
   } else {
+    // Redirect to splash page
     res.render('index.ejs', { randomGradient: req.randomGradient });
   }
-})
+});
 
+// Users Controller
 const usersController = require('./controllers/users_controller');
 app.use('/users', usersController);
 
+// Sessions Controller
 const sessionsController = require('./controllers/sessions_controller');
 app.use('/sessions', sessionsController);
 
+// Projects Controller
 const projectsController = require('./controllers/projects_controller');
 app.use('/projects', requireLogin, projectsController);
 
+// Colors Controller
 const colorsController = require('./controllers/colors_controller');
 app.use('/projects/:projectId/colors', requireLogin, colorsController);
